551bc00c98bceb5c55305c9b2a5a2a71b8a90dc1
张贵安 <zhangguian@chinasofti.com>
2023-01-05 18:59:23 +0800

    :memo: fix: 测试

--- app/router/novelTypeRoute.ts
+++ app/router/novelTypeRoute.ts
@@ -2,21 +2,9 @@ import { Application } from 'egg'
 
 export default (app: Application) => {
 	const { controller, router } = app
-	router.get(
-		'/queryPageTypeList',
-		controller.novelTypeController.queryPageNovelTypeList
-	)
-	router.get(
-		'/queryTypeList',
-		controller.novelTypeController.queryNovelTypeList
-	)
+	router.get('/queryPageTypeList', controller.novelTypeController.queryPageNovelTypeList)
+	router.get('/queryTypeList', controller.novelTypeController.queryNovelTypeList)
 	router.post('/addTypeInfo', controller.novelTypeController.addNovelTypeInfo)
-	router.post(
-		'/updateTypeInfo',
-		controller.novelTypeController.updateNovelType
-	)
-	router.post(
-		'/deleteTypeInfo',
-		controller.novelTypeController.deleteNovelType
-	)
+	router.post('/updateTypeInfo', controller.novelTypeController.updateNovelType)
+	router.post('/deleteTypeInfo', controller.novelTypeController.deleteNovelType)
 }
