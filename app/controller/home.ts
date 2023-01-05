7b5b133fa33a62d90c466639d49da610f89b7b52
张贵安 <zhangguian@chinasofti.com>
2023-01-05 19:02:52 +0800

    :memo: fix: 测试

--- app/controller/home.ts
+++ app/controller/home.ts
@@ -1,17 +1,17 @@
-import { Controller } from 'egg';
+import { Controller } from 'egg'
 
 export default class HomeController extends Controller {
 	public async login() {
-		const { ctx, app } = this;
+		const { ctx, app } = this
 		// 获取到客户端传过来的参数
-		const { username = '', password = '' } = ctx.request.body;
+		const { username = '', password = '' } = ctx.request.body
 		if (username !== '' && password !== '') {
 			const data = await ctx.model.User.findAll({
 				where: {
 					username,
 					password
 				}
-			});
+			})
 			if (data) {
 				const token = app.jwt.sign(
 					{
@@ -20,18 +20,18 @@ export default class HomeController extends Controller {
 						exp: Math.floor(Date.now() / 1000) + 24 * 60 * 60 // 有效期 24小时
 					},
 					app.config.jwt.secret
-				);
+				)
 				ctx.body = {
 					code: 200,
 					message: '登陆成功',
 					data: { token }
-				};
+				}
 			} else {
 				ctx.body = {
 					code: 200,
 					message: '用户名或者密码错误',
 					data: null
-				};
+				}
 			}
 		}
 	}
